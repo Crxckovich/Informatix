1bb4ea975bfec2f10dd17e863a502be2f4a6e7d2
Crxckovich <artpeshov281402@gmail.com>
2023-11-04 13:55:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# bukvi = input('Введите строку: ')
+# bukvi = bukvi.split()
+# revers_bukvi = ' '.join(bukvi[::-1])
+# print('ВО:', revers_bukvi)
+
+# Е
+
+# bukvi = input()
+# first_index = bukvi.find('f')
+# last_index = bukvi.rfind('f')
+
+# if first_index != -1:  
+#     if first_index == last_index:  
+#         print(first_index)
+#     else: 
+#         print(first_index, last_index)
+
+# F
+
+# G
+# bukvi = input()
+# first_h_index = bukvi.find('h')
+# last_h_index = bukvi.rfind('h')
+# result = bukvi[:first_h_index] + bukvi[last_h_index+1:]
+# print(result)
+
+#X
